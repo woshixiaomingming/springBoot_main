ab7de3e066ef411e3326adaa627eac620de1bd68
woshixiaomingming <986370232@qq.com>
2018-04-10 15:13:58 +0800

    one

---
+++ src/main/java/com/controller/Test.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+    @RequestMapping("/main")
+    public String test () {
+        return "ok";
+    }
+}
