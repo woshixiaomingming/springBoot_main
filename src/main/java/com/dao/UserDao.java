a520bcd8e6751d614ab12bcc42e22d4f8646b6f5
woshixiaomingming <986370232@qq.com>
2018-04-11 14:54:56 +0800

    ok

---
+++ src/main/java/com/dao/UserDao.java
@@ -0,0 +1,30 @@
+package com.dao;
+
+import com.model.User;
+import com.util.Data;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserDao extends Data {
+
+    public void insertUser () {
+        System.out.println(insert("insert user set name = ?", new Object[]{"xmmmmmmmm"}));
+    }
+
+    public static void main(String[] args) {
+        UserDao userDao = new UserDao();
+        userDao.insertUser();
+    }
+
+    /**
+     * 根据用户id查询用户信息
+     * @param id
+     * @return
+     */
+    public User findUserById (int id) {
+        String sql = "select * from user where id = ?";
+        return getOne(sql, new Object[]{id}, User.class);
+    }
+}
