cdc75b7da7c5071c84dd37223118cbe21a1af1d3
woshixiaomingming <986370232@qq.com>
2018-04-12 18:59:13 +0800

    ok

---
+++ src/main/java/com/entity/Message.java
@@ -0,0 +1,67 @@
+package com.entity;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Message {
+
+    private String code;
+
+    private String msg;
+
+    private boolean status = false;
+
+    private String data;
+
+    public Message (String code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Message (String code, String msg, boolean status) {
+        this.code = code;
+        this.msg = msg;
+        this.status = status;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public JSONObject getReturnInfo () {
+        JSONObject json = new JSONObject();
+        json.put("code", this.code);
+        json.put("msg", this.msg);
+        return json;
+    }
+}
