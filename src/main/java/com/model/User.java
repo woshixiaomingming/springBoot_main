053ab856e99b0fd989564fd1a678d09d45f910a3
woshixiaomingming <986370232@qq.com>
2018-04-12 18:59:53 +0800

    ok

--- src/main/java/com/model/User.java
+++ src/main/java/com/model/User.java
@@ -2,11 +2,93 @@ package com.model;
 
 import com.util.Bean;
 
+import java.sql.Timestamp;
+
 public class User extends Bean {
 
     private int id;
 
-    private String name;
+    private String username;
+
+    private Timestamp registerTime;
+
+    private String password;
+
+    private Timestamp lastLoginTime;
+
+    private int sex;
+
+    private String photo;
+
+    private String loginIp;
+
+    private boolean isDelete;
+
+    private int status;
+
+    public Timestamp getRegisterTime() {
+        return registerTime;
+    }
+
+    public void setRegisterTime(Timestamp registerTime) {
+        this.registerTime = registerTime;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Timestamp getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    public void setLastLoginTime(Timestamp lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getLoginIp() {
+        return loginIp;
+    }
+
+    public void setLoginIp(String loginIp) {
+        this.loginIp = loginIp;
+    }
+
+    public boolean isDelete() {
+        return isDelete;
+    }
+
+    public void setDelete(boolean delete) {
+        isDelete = delete;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 
     public int getId() {
         return id;
@@ -16,11 +98,11 @@ public class User extends Bean {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
